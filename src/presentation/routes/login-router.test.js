6757ee6a7b23fc20311f8b442404bc096811f030
Luan Martins <luan.cbm@hotmail.com>
2021-11-29 10:56:52 -0300

    att custom response

--- src/presentation/routes/login-router.test.js
+++ src/presentation/routes/login-router.test.js
@@ -5,11 +5,14 @@ class LoginRouter {
   route (httpRequest) {
     try {
       if (!httpRequest) {
-        throw new HttpError(500, '', 'Internal Server Error')
+        throw new HttpError(500, 'Unexpected Error!', 'Internal Server Error')
       }
       const { email, password } = httpRequest.body
-      if (!email || !password) {
-        throw new HttpError(400, '', 'badRequest')
+      if (!email) {
+        throw new HttpError(400, 'invalid: Email', 'badRequest')
+      }
+      if (!password) {
+        throw new HttpError(400, 'invalid: Password', 'badRequest')
       }
     } catch (error) {
       return HttpResponse.error(error)
@@ -27,6 +30,7 @@ describe('login router', () => {
     }
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body.message).toEqual('invalid: Email')
   })
 
   test('Should return 400 if no password is provided', () => {
@@ -38,6 +42,7 @@ describe('login router', () => {
     }
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body.message).toEqual('invalid: Password')
   })
 
   test('Should return 500 if no httpRequest is provided', () => {
