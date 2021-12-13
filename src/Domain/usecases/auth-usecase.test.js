202b224affdf6464f0da39344749afa9547557bd
Luan Martins <luan.cbm@hotmail.com>
2021-12-13 11:03:46 -0300

    teste:Should call LoadUserByEmailRepository with correct email

--- src/Domain/usecases/auth-usecase.test.js
+++ src/Domain/usecases/auth-usecase.test.js
@@ -1,6 +1,10 @@
 const Error = require('../../utils/error')
 
 class AuthUseCase {
+  constructor ({ loadUserByEmailRepository }) {
+    this.loadUserByEmailRepository = loadUserByEmailRepository
+  }
+
   async auth (email, password) {
     if (!email) {
       throw new Error(500)
@@ -9,15 +13,35 @@ class AuthUseCase {
       throw new Error(500)
     }
 
+    const user = await this.loadUserByEmailRepository.load(email)
+    console.log(user)
+
     return null
   }
 }
 
 const makeSut = () => {
-  const sut = new AuthUseCase()
+  const loadUserByEmailRepositorySpy = makeLoadUserByEmailRepository()
+  const sut = new AuthUseCase({ loadUserByEmailRepository: loadUserByEmailRepositorySpy })
   return {
+    loadUserByEmailRepositorySpy,
     sut
   }
+}
+
+const makeLoadUserByEmailRepository = () => {
+  class LoadUserByEmailRepositorySpy {
+    async load (email) {
+      this.email = email
+      return this.user
+    }
+  }
+  const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy()
+  loadUserByEmailRepositorySpy.user = {
+    id: 'any_id',
+    password: 'hashed_password'
+  }
+  return loadUserByEmailRepositorySpy
 }
 
 describe('Auth-usecase', () => {
@@ -33,5 +57,11 @@ describe('Auth-usecase', () => {
     const { sut } = makeSut()
     const promise = sut.auth('any_email@mail.com')
     expect(promise).rejects.toThrow(new Error(500, 'password'))
+  })
+
+  test('Should call LoadUserByEmailRepository with correct email', async () => {
+    const { sut, loadUserByEmailRepositorySpy } = makeSut()
+    await sut.auth('any_email@mail.com', 'any_password')
+    expect(loadUserByEmailRepositorySpy.email).toBe('any_email@mail.com')
   })
 })
