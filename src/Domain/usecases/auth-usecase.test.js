baab70484e962a8b20a5dd7319d9ce8945b15bd7
Luan Martins <luan.cbm@hotmail.com>
2021-12-13 10:07:19 -0300

    test:Shoul throw if no email is provided

---
+++ src/Domain/usecases/auth-usecase.test.js
@@ -0,0 +1,17 @@
+const Error = require('../../presentation/helpers/error')
+
+class AuthUseCase {
+  async auth (email, password) {
+    if (!email) {
+      throw new Error(500)
+    }
+  }
+}
+
+describe('Auth-usecase', () => {
+  test('Shoul throw if no email is provided', async () => {
+    const sut = new AuthUseCase()
+    const promise = sut.auth()
+    expect(promise).rejects.toThrow()
+  })
+})
