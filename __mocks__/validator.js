87ad5d3f04b6b2b6c4a17c81fc257821468ca119
Luan Martins <luan.cbm@hotmail.com>
2021-12-13 09:34:33 -0300

    teste: Should return false if validator return false

---
+++ __mocks__/validator.js
@@ -0,0 +1,8 @@
+module.exports = {
+  isEmailValid: true,
+
+  isEmail (email) {
+    console.log(this.isEmailValid)
+    return this.isEmailValid
+  }
+}
--- src/utils/email-validator.test.js
+++ src/utils/email-validator.test.js
@@ -1,13 +1,23 @@
+const validator = require('validator')
+
 class EmailValidator {
   isValid (email) {
-    return true
+    return validator.isEmail(email)
   }
 }
 
 describe('Email Validator', () => {
   test('Should return true if validator return true', () => {
     const sut = new EmailValidator()
+
     const response = sut.isValid('valid_email@meial.com')
     expect(response).toBe(true)
+  })
+
+  test('Should return false if validator return false', () => {
+    validator.isEmailValid = false
+    const sut = new EmailValidator()
+    const response = sut.isValid('invalid_email@meial.com')
+    expect(response).toBe(false)
   })
 })
