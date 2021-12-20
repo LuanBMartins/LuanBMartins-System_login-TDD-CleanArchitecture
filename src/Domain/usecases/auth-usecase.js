3f6ae97d8ab35f69e9d0ba6dbb5cba6cbab7682f
Luan Martins <luan.cbm@hotmail.com>
2021-12-20 09:48:10 -0300

    move AuthUseCase to its own file

---
+++ src/Domain/usecases/auth-usecase.js
@@ -0,0 +1,24 @@
+const Error = require('../../utils/error')
+
+module.exports = class AuthUseCase {
+  constructor ({ loadUserByEmailRepository }) {
+    this.loadUserByEmailRepository = loadUserByEmailRepository
+  }
+
+  async auth (email, password) {
+    if (!email) {
+      throw new Error(500)
+    }
+    if (!password) {
+      throw new Error(500)
+    }
+
+    const user = await this.loadUserByEmailRepository.load(email)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+}
--- src/Domain/usecases/auth-usecase.test.js
+++ src/Domain/usecases/auth-usecase.test.js
@@ -1,27 +1,5 @@
 const Error = require('../../utils/error')
-
-class AuthUseCase {
-  constructor ({ loadUserByEmailRepository }) {
-    this.loadUserByEmailRepository = loadUserByEmailRepository
-  }
-
-  async auth (email, password) {
-    if (!email) {
-      throw new Error(500)
-    }
-    if (!password) {
-      throw new Error(500)
-    }
-
-    const user = await this.loadUserByEmailRepository.load(email)
-
-    if (!user) {
-      return null
-    }
-
-    return user
-  }
-}
+const AuthUseCase = require('./auth-usecase')
 
 const makeSut = () => {
   const loadUserByEmailRepositorySpy = makeLoadUserByEmailRepository()
