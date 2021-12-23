1d1d29d23a25f55110ede75bd9e57da9fd107594
Luan Martins <luan.cbm@hotmail.com>
2021-12-23 09:33:40 -0300

    add test:Should call bcrypt with correct values

--- __mocks__/bcrypt.js
+++ __mocks__/bcrypt.js
@@ -1,6 +1,8 @@
 module.exports = {
   isValid: true,
   async compare (value, hashedValue) {
+    this.value = value
+    this.hash = hashedValue
     return this.isValid
   }
 }
--- src/utils/encrypter.test.js
+++ src/utils/encrypter.test.js
@@ -25,8 +25,14 @@ describe('Encrypter', () => {
   test('Should return false if bcrypt returns false', async () => {
     const { sut } = makeSut()
     bcrypt.isValid = false
-    sut.isValid = false
     const isValid = await sut.compare('any_value', 'hashed_value')
     expect(isValid).toBe(false)
+  })
+
+  test('Should call bcrypt with correct values', async () => {
+    const { sut } = makeSut()
+    await sut.compare('any_value', 'hashed_value')
+    expect(bcrypt.value).toBe('any_value')
+    expect(bcrypt.hash).toBe('hashed_value')
   })
 })
